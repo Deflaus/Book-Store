d11febd46085644faafa81596f937a3b9e40592d
Deflaus <snow41521@gmail.com>
2021-01-26 11:25:07 +0300

    adding models Book Author Order

--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('full_name',)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('title',)
+    
+    def __str__(self):
+        return self.title
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
+    phone_number = models.CharField(max_length=11)
+    comment = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order {self.id}'
